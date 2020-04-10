e1b406035be9c9cf5ce49e38cac7b9696d2bf21f
You Song Hou <yousonghou@yahoo.com>
2020-04-10 01:02:05 -0400

    refactor categories page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,31 +2,55 @@ import React from 'react';
 
 import ExImg from "../img/example1.jpeg";
 
+const list = [
+    {
+        id: 1,
+        imageS: ExImg
+    },
+    {
+        id: 2,
+        imageS: ExImg
+    },
+    {
+        id: 3,
+        imageS: ExImg
+    },
+    {
+        id: 4,
+        imageS: ExImg
+    },
+    {
+        id: 5,
+        imageS: ExImg
+    },
+    {
+        id: 6,
+        imageS: ExImg
+    },
+    {
+        id: 7,
+        imageS: ExImg
+    },
+    {
+        id: 8,
+        imageS: ExImg
+    },
+]
+
 const Categories = () => {
     return(
         <div className="container mt-3">
             <h1 className="text-center">Categories</h1>
             <div className="row">
-                <div className="col-6 col-sm-6 my-2">
-                    <div class="card">
-                        <img src={ExImg} class="card-img-top" alt="Categoeries #1" />
-                    </div>
-                </div>
-                <div className="col-6 col-sm-6 my-2">
-                    <div class="card">
-                        <img src={ExImg} class="card-img-top" alt="Categoeries #1" />
-                    </div>
-                </div>
-                <div className="col-6 col-sm-6 my-2">
-                    <div class="card">
-                        <img src={ExImg} class="card-img-top" alt="Categoeries #1" />
-                    </div>
-                </div>
-                <div className="col-6 col-sm-6 my-2">
-                    <div class="card">
-                        <img src={ExImg} class="card-img-top" alt="Categoeries #1" />
-                    </div>
-                </div>
+                {list.map(item => {
+                    return(
+                        <div className="col-6 col-md-4 my-2">
+                            <div class="card" key={item.id}>
+                                <img src={item.imageS} class="card-img-top" alt={`Categoeries  #${item.id}`} />
+                            </div>
+                        </div>
+                    )
+                })} 
             </div>
             
         </div>
