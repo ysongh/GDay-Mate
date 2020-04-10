b2c9697847f29144b172bf3186470900cfa8d8fa
You Song Hou <yousonghou@yahoo.com>
2020-04-10 19:08:38 -0400

    add a get request to get company information

--- src/components/OrganizationProfile.js
+++ src/components/OrganizationProfile.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams} from "react-router";
 
 import defaultLogo from '../img/defaultCompanyLogo.png';
 
 const OrganizationProfile = () => {
+    const [data, setData] = useState({
+        "id": 0,
+        "name": "Company Name",
+        "location": "Location",
+        "services": []
+    })
+
+    const {id} =  useParams();
+
+    useEffect(() => {
+        const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations/${id}`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(resData => {
+                setData(resData);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
     return(
         <>
             <div className="bg-primary p-4">
                 <img className="w-25 rounded-circle m-center" src={defaultLogo} alt="Company Logo" />
-                <h1 className="mt-2">Company Name</h1>
-                <p>Groceries, Cleaning</p>
-                <p className="mb-4">Location</p>
+                <h1 className="mt-2">{data.name}</h1>
+                <p>
+                    {data.services.map(i => i + ", ")}
+                </p>
+                <p className="mb-4">{data.location}</p>
             </div>
             <div className="container orgainization__card">
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="card-title mb-4">Individuals who might need your help</h2>
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="card-title mb-4">Individuals who might need your help</h2>
                         <p>&#9786; Bob Park</p>
                         <p>&#9786; Joe Doe</p>
                         <p>&#9786; Rob Car</p>
