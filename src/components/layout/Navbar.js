e53432a8386c8598a64099e8ff722cc422b227d8
You Song Hou <yousonghou@yahoo.com>
2020-04-09 23:27:20 -0400

    replace href with Link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -10,13 +12,13 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Home</a>
+                        <Link className="nav-link" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/login">Login</a>
+                        <Link className="nav-link" to="/login">Login</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/register">Register</a>
+                        <Link className="nav-link" to="/register">Register</Link>
                     </li>
                 </ul>
             </div>
