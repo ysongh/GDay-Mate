5e7944769c120da8fca10983e4a908909a1206af
electrone901 <carbajalluis@rocketmail.com>
2020-04-11 00:04:51 -0400

    finished companies

--- src/components/Organization.js
+++ src/components/Organization.js
@@ -1,102 +1,117 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import Spinner from './common/Spinner';
+import Spinner from "./common/Spinner";
 
 const servicesList = ["Groceries", "Cleaning", "Social", "Depression"];
 
 const Organization = () => {
-    const [name, setName] = useState("");
-    const [services, setServices] = useState([]);
-    const [location, setlocation] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [services, setServices] = useState([]);
+  const [location, setlocation] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    // for redirecting
-    const history = useHistory();
+  // for redirecting
+  const history = useHistory();
 
-    const onSubmit = e => {
-        e.preventDefault();
-        setLoading(true);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-        const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations`;
+    const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations`;
 
-        fetch(url, {
-                method: 'POST',
-                body: JSON.stringify({
-                name: name,
-                services: services,
-                location: location
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(resData => {
-                history.push("/organization/profile/" + resData.id);
-            })
-            .catch(err => {
-                console.log(err);
-                setLoading(false);
-            });
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        services: services,
+        location: location,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        history.push("/organization/profile/" + resData.id);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  const handleCheckBoxservices = (e) => {
+    const newSelection = e.target.value;
+    let newSelectionArray;
+
+    if (services.indexOf(newSelection) > -1) {
+      newSelectionArray = services.filter((s) => s !== newSelection);
+    } else {
+      newSelectionArray = [...services, newSelection];
     }
 
-    const handleCheckBoxservices = e => {
-        const newSelection = e.target.value;
-        let newSelectionArray;
+    setServices(newSelectionArray);
+  };
 
-        if (services.indexOf(newSelection) > -1) {
-            newSelectionArray = services.filter(s => s !== newSelection);
-        } else {
-            newSelectionArray = [...services, newSelection];
-        }
-
-        setServices(newSelectionArray);
-    }
-    
-    return(
-        <div className="container">
-            <form className="mt-5 p-4 bg-light">
-                <h1 className="mb-4">Organization Sign Up</h1>
-                <div className="form-group">
-                    <label htmlFor="name">Name of Organization</label>
-                    <input
-                        type="text"
-                        name="name"
-                        className="form-control"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="services">Services</label><br />
-                    {servicesList.map(service => {
-                        return(
-                            <div className="form-check form-check-inline" key={service}>
-                                <input
-                                    className="form-check-input"
-                                    type="checkbox"
-                                    id={service}
-                                    value={service}
-                                    onChange={e => handleCheckBoxservices(e)}/>
-                                <label className="form-check-label" htmlFor={service}>{service}</label>
-                            </div>
-                        )
-                    })}
-                </div>
-                
-                <div className="form-group">
-                    <label htmlFor="description">Location</label>
-                    <input
-                        type="text"
-                        id="description"
-                        className="form-control"
-                        value={location} 
-                        onChange={(e) => setlocation(e.target.value)}/>
-                </div>
-                {loading ? <Spinner /> : <input type="submit" className="btn btn-primary btn-block" value="Sign Up" onClick={e => onSubmit(e)} />}
-            </form>
+  return (
+    <div className="container">
+      <form className="mt-5 p-4 bg-light">
+        <h1 className="mb-4">Organization Sign Up</h1>
+        <div className="form-group">
+          <label htmlFor="name">Name of Organization</label>
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    );
+        <div className="form-group">
+          <label htmlFor="services">Services</label>
+          <br />
+          {servicesList.map((service) => {
+            return (
+              <div className="form-check form-check-inline" key={service}>
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={service}
+                  value={service}
+                  onChange={(e) => handleCheckBoxservices(e)}
+                />
+                <label className="form-check-label" htmlFor={service}>
+                  {service}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="description">Location</label>
+          <input
+            type="text"
+            id="description"
+            className="form-control"
+            value={location}
+            onChange={(e) => setlocation(e.target.value)}
+          />
+        </div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <input
+            type="submit"
+            className="btn btn-info  btn-block"
+            value="Sign Up"
+            onClick={(e) => onSubmit(e)}
+          />
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default Organization;
--- src/components/OrganizationProfile.js
+++ src/components/OrganizationProfile.js
@@ -1,78 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { useParams} from "react-router";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-import defaultLogo from '../img/defaultCompanyLogo.png';
+// import defaultLogo from "../img/defaultCompanyLogo.png";
+import defaultLogo from "../img/c2.jpg";
 
 const OrganizationProfile = () => {
-    const [data, setData] = useState({
-        "id": 0,
-        "name": "Company Name",
-        "location": "Location",
-        "services": []
-    })
+  const [data, setData] = useState({
+    id: 0,
+    name: "Company Name",
+    location: "Location",
+    services: [],
+  });
 
-    const [usersData, setUsersData] = useState([]);
+  const [usersData, setUsersData] = useState([]);
 
-    const {id} =  useParams();
+  const { id } = useParams();
 
-    useEffect(() => {
-        const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations/${id}`;
+  useEffect(() => {
+    const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations/${id}`;
 
-        fetch(url)
-            .then(res => res.json())
-            .then(resData => {
-                setData(resData);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);
+    fetch(url)
+      .then((res) => res.json())
+      .then((resData) => {
+        setData(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations/${id}/matches`;
+  useEffect(() => {
+    const url = `https://gdaymatebackend.azurewebsites.net/api/Organisations/${id}/matches`;
 
-        fetch(url)
-            .then(res => res.json())
-            .then(resData => {
-                setUsersData(resData);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);
+    fetch(url)
+      .then((res) => res.json())
+      .then((resData) => {
+        setUsersData(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-
-    return(
-        <>
-            <div className="bg-primary p-4">
-                <img className="default__img rounded-circle m-center" src={defaultLogo} alt="Company Logo" />
-                <h1 className="mt-2">{data.name}</h1>
-                <p>
-                    {data.services.map(i => i + ", ")}
-                </p>
-                <p className="mb-4">{data.location}</p>
-            </div>
-            <div className="container orgainization__card">
-                <div className="card">
-                    <div className="card-body">
-                        <h2 className="card-title mb-4">Individuals who might need your help</h2>
-                        {usersData.length > 0 ? (
-                            usersData.map(user => {
-                                return (
-                                    <Link key={user.id} to={`/user/profile/${user.id}`}>
-                                        <p >&#9786; {user.firstName} {user.lastName}</p>
-                                    </Link>
-                                    
-                                )
-                            })
-                        ) : <p>No match yet!</p>}
-                    </div>
-                </div>
-            </div>
-            
-        </>
-    );
+  return (
+    <>
+      <div className="p-4 cardHeader">
+        <img
+          className="default__img rounded-circle m-center"
+          src={defaultLogo}
+          alt="Company Logo"
+        />
+        <h1 className="mt-2">{data.name}</h1>
+        <p className="title-white">{data.services.map((i) => i + ", ")}</p>
+        <p className="title-white">{data.location}</p>
+      </div>
+      <div className="container orgainization__card">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="card-title">Individuals who might need your help</h2>
+            {usersData.length > 0 ? (
+              usersData.map((user) => {
+                return (
+                  <Link key={user.id} to={`/user/profile/${user.id}`}>
+                    <p>
+                      &#9786; {user.firstName} {user.lastName}
+                    </p>
+                  </Link>
+                );
+              })
+            ) : (
+              <p>No match yet!</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrganizationProfile;
