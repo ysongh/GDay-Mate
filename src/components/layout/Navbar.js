1288447477c40eefbf870dbac56a36455e5507c4
You Song Hou <yousonghou@yahoo.com>
2020-04-10 22:17:10 -0400

    change logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Logo from  '../../img/logo.png';
+import Logo from  '../../img/logo.svg';
 
 const Navbar = () => {
     
